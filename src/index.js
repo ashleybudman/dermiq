be0c164a2dbc00aa06e9ede48a8bfa429461e260
Ashley Budman <ashleybudman@umail.ucsb.edu>
2024-02-20 11:37:58 -0800

    fix index

--- src/index.js
+++ src/index.js
@@ -4,9 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Amplify } from 'aws-amplify';
-import amplifyconfig from './amplifyconfiguration.json';
-
-Amplify.configure(amplifyconfig);
+import config from './aws-exports';
+Amplify.configure(config);
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
